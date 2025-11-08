9fd8fc98aa48b818f667b96f28e6fa4b34349071
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-11-08 13:48:22 +0800

    change the import to use my code changes

--- src/app/recruiter-dashboard/careers/new-career/page.tsx
+++ src/app/recruiter-dashboard/careers/new-career/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import HeaderBar from "@/lib/PageComponent/HeaderBar";
-import CareerForm from "@/lib/components/CareerComponents/CareerForm";
+import CareerFormV2 from "@/lib/components/CareerComponents/CareerFormV2";
 
 export default function NewCareerPage() {
     return (
@@ -10,7 +10,7 @@ export default function NewCareerPage() {
         <HeaderBar activeLink="Careers" currentPage="Add new career" icon="la la-suitcase" />
         <div className="container-fluid mt--7" style={{ paddingTop: "6rem" }}>
           <div className="row">
-            <CareerForm formType="add" />
+            <CareerFormV2 formType="add" />
           </div>
         </div>
       </>
